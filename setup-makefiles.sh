edbc3a416cfcd4c81b93e8fce575a06a5db9d1d7
dd3boh <dade.garberi@gmail.com>
2017-11-01 14:46:09 +0100

    z2_plus: Commonize the vendor
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -20,48 +20,9 @@ set -e
 
 # Required!
 export DEVICE=z2_plus
+export DEVICE_COMMON=msm8996-common
 export VENDOR=zuk
 
 export DEVICE_BRINGUP_YEAR=2016
 
-INITIAL_COPYRIGHT_YEAR=2017
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper for common
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
-
-# Copyright headers and guards
-write_headers "z2_plus"
-
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# We are done!
-write_footers
-
-if [ -s "$MY_DIR"/../"$DEVICE"/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../"$DEVICE"/proprietary-files.txt
-
-    # We are done!
-    write_footers
-fi
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
