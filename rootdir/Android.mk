551780c0117c3978cf657f9903927dff4ba0dc0f
Xpsoted <37337416+Xpsoted@users.noreply.github.com>
2021-08-04 21:42:29 +0800

    zuk msm8996 device

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,11 +1,11 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-# Device specific init script
+# Device init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE	   := init.zuk.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.zuk.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+LOCAL_MODULE		:= init.zuk.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.zuk.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
