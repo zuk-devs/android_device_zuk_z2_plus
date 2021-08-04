300a6b283638fb8249d849e05e5ac81811680b82
Xpsoted <37337416+Xpsoted@users.noreply.github.com>
2021-08-04 21:38:07 +0800

    2016-2017

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -23,6 +23,6 @@ export DEVICE=z2x
 export DEVICE_COMMON=msm8996-common
 export VENDOR=zuk
 
-export DEVICE_BRINGUP_YEAR=2016
+export DEVICE_BRINGUP_YEAR=2017
 
 source "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
